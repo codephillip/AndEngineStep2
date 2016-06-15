fc8be1762b4895756c8db1cd353be8a31ea785af
codephillip <codephillip@gmail.com>
2016-06-15 17:22:41 +0300

    added circle

--- app/src/main/java/com/codephillip/andenginestep/MainActivity.java
+++ app/src/main/java/com/codephillip/andenginestep/MainActivity.java
@@ -3,9 +3,15 @@ package com.codephillip.andenginestep;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
-import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.adt.color.Color;
 
@@ -16,16 +22,27 @@ public class MainActivity extends SimpleBaseGameActivity {
     private Camera camera;
 
     private static int CAMERA_WIDTH = 480;
-    private static int CAMERA_LENGTH = 800;
+    private static int CAMERA_LENGTH = 640;
+
+    private BitmapTextureAtlas bitmapTextureAtlas;
+    private TextureRegion circleRegion;
+    private Sprite sprite;
 
     @Override
     protected void onCreateResources() throws IOException {
-
+        //create BitmapTextureAtlas of size 256*256
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        bitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 256, 256, TextureOptions.DEFAULT);
+        circleRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bitmapTextureAtlas, this,"circle.png",0 ,0 );
+        bitmapTextureAtlas.load();
     }
 
     @Override
     protected Scene onCreateScene() {
+        this.mEngine.registerUpdateHandler(new FPSLogger());
+        sprite = new Sprite(300, 300, circleRegion, this.getVertexBufferObjectManager());
         Scene scene = new Scene();
+        scene.attachChild(sprite);
         scene.setBackground(new Background(Color.GREEN));
         return scene;
     }
@@ -33,7 +50,7 @@ public class MainActivity extends SimpleBaseGameActivity {
     @Override
     public EngineOptions onCreateEngineOptions() {
         camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_LENGTH);
-        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_LENGTH), camera);
         return engineOptions;
     }
 }
