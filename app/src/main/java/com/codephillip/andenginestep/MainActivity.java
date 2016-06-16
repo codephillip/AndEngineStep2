fdc6cb6ad0176d6adc48fd4643c9ca4fa333c3c5
codephillip <codephillip@gmail.com>
2016-06-16 16:15:54 +0300

    added arrow and marble

--- app/src/main/java/com/codephillip/andenginestep/MainActivity.java
+++ app/src/main/java/com/codephillip/andenginestep/MainActivity.java
@@ -1,44 +1,47 @@
 package com.codephillip.andenginestep;
 
-import android.graphics.Typeface;
-
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
-import org.andengine.entity.text.Text;
-import org.andengine.opengl.font.Font;
-import org.andengine.opengl.font.FontFactory;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.adt.color.Color;
+import org.andengine.util.math.MathUtils;
 
-import java.util.Calendar;
-
-public class MainActivity extends BaseGameActivity {
+public class MainActivity extends BaseGameActivity implements IOnSceneTouchListener {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 480;
 
-    private static final String TIME_STRING_PREFIX = "Time: ";
-    private static final String TIME_FORMAT = "00:00:00";
-
-    /* Obtain the maximum number of characters that our Text
-     * object will need to display*/
-    private static final int MAX_CHARACTER_COUNT = TIME_STRING_PREFIX.length() + TIME_FORMAT.length();
+    private static final int DEFAULT_IMAGE_ROTATION = 90;
 
     private Scene mScene;
     private Camera mCamera;
 
-    private Font mFont;
+    private Sprite mArrowSprite;
+    private Sprite mMarbleSprite;
+
+    private ITextureRegion mArrowTextureRegion;
+    private ITextureRegion mMarbleTextureRegion;
 
     @Override
     public EngineOptions onCreateEngineOptions() {
         mCamera = new Camera(0, 0, WIDTH, HEIGHT);
 
-        EngineOptions engineOptions = new EngineOptions(true,
-                ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(),
-                mCamera);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), mCamera);
 
         return engineOptions;
     }
@@ -46,19 +49,19 @@ public class MainActivity extends BaseGameActivity {
     @Override
     public void onCreateResources(
             OnCreateResourcesCallback pOnCreateResourcesCallback) {
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 
-        mFont = FontFactory.create(mEngine.getFontManager(),
-                mEngine.getTextureManager(), 256, 256,
-                Typeface.create(Typeface.DEFAULT, Typeface.NORMAL), 32f, true,
-                Color.WHITE_ABGR_PACKED_INT);
-        mFont.load();
+        BuildableBitmapTextureAtlas mBitmapTextureAtlas = new BuildableBitmapTextureAtlas(mEngine.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
 
-		/*
-		 * Prepare the mFont object for the most common characters used. This
-		 * will eliminate the need for the garbage collector to run when using a
-		 * letter/number that's never been used before
-		 */
-        mFont.prepareLetters("Time: 1234567890".toCharArray());
+        mArrowTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, this, "down_arrow_reverse.png");
+        mMarbleTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, this, "marble.png");
+
+        try {
+            mBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 1, 1));
+        } catch (ITextureAtlasBuilder.TextureAtlasBuilderException e) {
+            e.printStackTrace();
+        }
+        mBitmapTextureAtlas.load();
 
         pOnCreateResourcesCallback.onCreateResourcesFinished();
     }
@@ -67,6 +70,9 @@ public class MainActivity extends BaseGameActivity {
     public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) {
         mScene = new Scene();
 
+		/* Register this activity as the mScene's touch listener */
+        mScene.setOnSceneTouchListener(this);
+
         pOnCreateSceneCallback.onCreateSceneFinished(mScene);
     }
 
@@ -74,77 +80,49 @@ public class MainActivity extends BaseGameActivity {
     public void onPopulateScene(Scene pScene,
                                 OnPopulateSceneCallback pOnPopulateSceneCallback) {
 
-		/*
-		 * The height never changes as the time ticks, so we can set a final
-		 * value
-		 */
-        final float timeTextHeight = HEIGHT - (mFont.getLineHeight() * 0.5f);
+		/* Add our marble sprite to the bottom left side of the Scene initially */
+        mMarbleSprite = new Sprite(mMarbleTextureRegion.getWidth(), mMarbleTextureRegion.getHeight(), mMarbleTextureRegion, mEngine.getVertexBufferObjectManager());
 
-		/* Create the time Text object which will update itself as time passes */
-        Text mTimeText = new Text(0, timeTextHeight, mFont, TIME_STRING_PREFIX
-                + TIME_FORMAT, MAX_CHARACTER_COUNT, mEngine.getVertexBufferObjectManager()) {
+		/* Attach the marble to the Scene */
+        mScene.attachChild(mMarbleSprite);
 
-            /*
-             * In order to avoid unnecessary updates, we will compare the value
-             * of the current second with that of the last update to the Text
-             * entity
-             */
-            int lastSecond = 0;
+		/* Create the arrow sprite and center it in the Scene */
+        mArrowSprite = new Sprite(WIDTH * 0.5f, HEIGHT * 0.5f, mArrowTextureRegion, mEngine.getVertexBufferObjectManager());
 
-            @Override
-            protected void onManagedUpdate(float pSecondsElapsed) {
-
-                Calendar c = Calendar.getInstance();
-
-				/*
-				 * We will only obtain the second for now in order to verify
-				 * that it's time to update the Text's string
-				 */
-                final int second = c.get(Calendar.SECOND);
-
-				/*
-				 * If the last update's second value is not equal to the
-				 * current...
-				 */
-                if (lastSecond != second) {
-
-					/* Obtain the new hour and minute time values */
-                    final int hour = c.get(Calendar.HOUR);
-                    final int minute = c.get(Calendar.MINUTE);
-
-					/* also, update the latest second value */
-                    lastSecond = second;
-
-					/* Build a new string with the current time */
-                    final String timeTextSuffix = hour + ":" + minute + ":"
-                            + second;
-
-					/* Set the Text object's string to that of the new time */
-                    this.setText(TIME_STRING_PREFIX + timeTextSuffix);
-
-					/*
-					 * Since the width of the Text will change with every change
-					 * in second, we should realign the Text position to the
-					 * edge of the screen minus half the Text's width
-					 */
-                    this.setX(WIDTH - this.getWidth() * 0.5f);
-                }
-
-                super.onManagedUpdate(pSecondsElapsed);
-            }
-
-        };
-
-		/* Change the color of the Text to blue */
-        mTimeText.setColor(0, 0, 1);
-
-		/* Attach the Text object to the Scene */
-        mScene.attachChild(mTimeText);
+		/* Attach the arrow to the Scene */
+        mScene.attachChild(mArrowSprite);
+        mScene.setBackground(new Background(Color.WHITE));
 
         pOnPopulateSceneCallback.onPopulateSceneFinished();
     }
-}
 
+    @Override
+    public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+        // If a user moves their finger on the device
+        if(pSceneTouchEvent.isActionMove()){
+
+			/* Set the marble's position to that of the touch even coordinates */
+            mMarbleSprite.setPosition(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+
+			/* Calculate the difference between the two sprites x and y coordinates */
+            final float dX = mMarbleSprite.getX() - mArrowSprite.getX();
+            final float dY = mMarbleSprite.getY() - mArrowSprite.getY();
+
+			/* Calculate the angle of rotation in radians*/
+            final float angle = (float) Math.atan2(-dY, dX);
+
+			/* Convert the angle from radians to degrees, adding the default image rotation */
+            final float rotation = MathUtils.radToDeg(angle) + DEFAULT_IMAGE_ROTATION;
+
+			/* Set the arrow's new rotation */
+            mArrowSprite.setRotation(rotation);
+
+            return true;
+        }
+
+        return false;
+    }
+}
 
 //    private Camera camera;
 //
