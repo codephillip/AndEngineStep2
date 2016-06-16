b28e980f3b18f2c517c16cbd00a70f995ab375a8
codephillip <codephillip@gmail.com>
2016-06-16 12:20:55 +0300

    added background image

--- app/src/main/java/com/codephillip/andenginestep/MainActivity.java
+++ app/src/main/java/com/codephillip/andenginestep/MainActivity.java
@@ -5,9 +5,12 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
-import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
-import org.andengine.util.adt.color.Color;
 
 import java.io.IOException;
 
@@ -15,18 +18,41 @@ public class MainActivity extends SimpleBaseGameActivity {
 
     private Camera camera;
 
-    private static int CAMERA_WIDTH = 480;
-    private static int CAMERA_LENGTH = 800;
+    private static int CAMERA_WIDTH = 800;
+    private static int CAMERA_LENGTH = 480;
+
+//    private BuildableBitmapTextureAtlas buildableBitmapTextureAtlas;
+    private BitmapTextureAtlas bitmapTextureAtlas;
+    private ITextureRegion iTextureRegion;
 
     @Override
     protected void onCreateResources() throws IOException {
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        //BuildableBitmapTextureAtlas auto scales
+//        buildableBitmapTextureAtlas = new BuildableBitmapTextureAtlas(mEngine.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR);
+        bitmapTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 1024, 1024, TextureOptions.DEFAULT);
+        iTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bitmapTextureAtlas, this, "background1024.jpg", 0, 0);
+        bitmapTextureAtlas.load();
 
+//        try {
+//            buildableBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0,1,1));
+//        } catch (ITextureAtlasBuilder.TextureAtlasBuilderException e) {
+//            e.printStackTrace();
+//        }
+//
+//        buildableBitmapTextureAtlas.load();
     }
 
     @Override
     protected Scene onCreateScene() {
+
+        final float positionX = CAMERA_WIDTH * 0.5f;
+        final float positionY = CAMERA_LENGTH * 0.5f;
+        Sprite sprite = new Sprite(positionX, positionY, iTextureRegion, mEngine.getVertexBufferObjectManager());
+
         Scene scene = new Scene();
-        scene.setBackground(new Background(Color.GREEN));
+        scene.attachChild(sprite);
+//        scene.setBackground(new Background(Color.GREEN));
         return scene;
     }
 
