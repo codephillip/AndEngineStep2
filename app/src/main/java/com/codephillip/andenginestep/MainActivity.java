a2ffc0247826fc9821df93588fa0824a61179da8
codephillip <codephillip@gmail.com>
2016-06-18 14:32:59 +0300

    added EntityBackground

--- app/src/main/java/com/codephillip/andenginestep/MainActivity.java
+++ app/src/main/java/com/codephillip/andenginestep/MainActivity.java
@@ -1,59 +1,36 @@
 package com.codephillip.andenginestep;
 
-import android.util.Log;
-
-import org.andengine.audio.music.Music;
-import org.andengine.audio.music.MusicFactory;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.Entity;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
-import org.andengine.entity.sprite.TiledSprite;
-import org.andengine.input.touch.TouchEvent;
-import org.andengine.opengl.texture.TextureOptions;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
-import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.entity.scene.background.EntityBackground;
 import org.andengine.ui.activity.BaseGameActivity;
 
 import java.io.IOException;
 
 public class MainActivity extends BaseGameActivity {
 
-    private static final String TAG = "MUTED";
+    private static final String TAG = "background";
     private Camera camera;
     private Scene scene;
     private static int CAMERA_WIDTH = 800;
     private static int CAMERA_HEIGHT = 480;
 
-    private static final int MUTED = 1;
-    private static final int UNMUTED = 0;
-
-    private ITiledTextureRegion buttonTextureRegion;
-    private BitmapTextureAtlas bitmapTextureAtlas;
-    private TiledSprite muteTiledSprite;
-
-    private Music music;
 
 
     @Override
     public EngineOptions onCreateEngineOptions() {
         camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
         EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
-        engineOptions.getAudioOptions().setNeedsSound(true);
-        engineOptions.getAudioOptions().setNeedsMusic(true);
         return engineOptions;
     }
 
     @Override
     public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException {
-        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-        bitmapTextureAtlas = new BitmapTextureAtlas(mEngine.getTextureManager(), 64, 32, TextureOptions.BILINEAR);
-        buttonTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(bitmapTextureAtlas, this, "faces.png",0 ,0 , 2, 1);
-        bitmapTextureAtlas.load();
-
-        music = MusicFactory.createMusicFromAsset(mEngine.getMusicManager(), this, "mfx/gamemenu.mp3");
         pOnCreateResourcesCallback.onCreateResourcesFinished();
     }
 
@@ -65,57 +42,30 @@ public class MainActivity extends BaseGameActivity {
 
     @Override
     public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException {
-        muteTiledSprite = new TiledSprite(CAMERA_WIDTH * 0.5f, CAMERA_HEIGHT * 0.5f, buttonTextureRegion, mEngine.getVertexBufferObjectManager()){
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
 
-                if(pSceneTouchEvent.isActionDown()){
-                    if (music.isPlaying()){
-                        music.pause();
-                        muteTiledSprite.setCurrentTileIndex(MUTED);
-                    } else {
-                        music.play();
-                        muteTiledSprite.setCurrentTileIndex(UNMUTED);
-                    }
-                    return true;
-                }
-                return super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
-            }
-        };
+        /* Create a rectangle in the bottom left corner of the Scene */
+        Rectangle rectangleLeft = new Rectangle(100, 100, 200, 200, mEngine.getVertexBufferObjectManager());
+/* Create a rectangle in the top right corner of the Scene */
+        Rectangle rectangleRight = new Rectangle(CAMERA_WIDTH - 100, CAMERA_HEIGHT - 100, 200, 200, mEngine.getVertexBufferObjectManager());
 
-        muteTiledSprite.setCurrentTileIndex(UNMUTED);
-        music.setLooping(true);
-        music.play();
+        Entity backgroundEntity = new Entity();
 
-        scene.registerTouchArea(muteTiledSprite);
-        scene.attachChild(muteTiledSprite);
+        backgroundEntity.attachChild(rectangleLeft);
+        backgroundEntity.attachChild(rectangleRight);
+
+        /* Define the background color properties */
+        final float red = 0;
+        final float green = 1;
+        final float blue = 0;
+/* Create the EntityBackground, specifying its background color &
+entity to represent the background image */
+        EntityBackground background = new EntityBackground(red, green,
+                blue, backgroundEntity);
+/* Set & enable the background */
+        scene.setBackground(background);
+        scene.setBackgroundEnabled(true);
+
         pOnPopulateSceneCallback.onPopulateSceneFinished();
-    }
-
-    @Override
-    public synchronized void onResumeGame() {
-        super.onResumeGame();
-        Log.d(TAG, "onResumeGame: ");
-
-        /* If the music and button have been created */
-        if (music != null && muteTiledSprite != null) {
-			/* If the mMuteButton is set to unmuted on resume... */
-            if(muteTiledSprite.getCurrentTileIndex() == UNMUTED){
-				/* Play the menu music */
-                music.play();
-            }
-        }
-    }
-
-    @Override
-    public synchronized void onPauseGame() {
-        super.onPauseGame();
-        Log.d(TAG, "onPauseGame: ");
-
-        /* Always pause the music on pause */
-        if(muteTiledSprite != null && music.isPlaying()){
-            music.pause();
-        }
     }
 }
 
